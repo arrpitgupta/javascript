c6cf85ad0f29eb7b5845a5425c6456c0a105b80b
arrpitgupta <arpit17200@gmail.com>
2025-01-30 18:34:13 +0530

    added ts

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
 //hey there will practice typscript here!!
+function greet(name: string, callback: (message: string) => void) {
+    const message = `Hello, ${name}!`;
+    callback(message);
+}
+
+greet("Alice", (msg) => {
+    console.log(msg);
+});
