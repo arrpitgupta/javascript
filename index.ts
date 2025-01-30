2510f95da574a7457b8fbffdaa5d9ca7a00579bb
arrpitgupta <arpit17200@gmail.com>
2025-01-30 18:43:56 +0530

    ts

--- index.ts
+++ index.ts
@@ -1,9 +1,2 @@
 //hey there will practice typscript here!!
-function greet(name: string, callback: (message: string) => void) {
-    const message = `Hello, ${name}!`;
-    callback(message);
-}
 
-greet("Alice", (msg) => {
-    console.log(msg);
-});
